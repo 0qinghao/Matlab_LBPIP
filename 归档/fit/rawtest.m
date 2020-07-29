e8a8cff85cbe252f2b5cd0452f006e1afac295bb
0qinghao <shu_rin@shu.edu.cn>
2020-07-29 13:54:49 +0800

    线性拟合效果很差

--- 归档/fit/rawtest.m
+++ 归档/fit/rawtest.m
@@ -1,52 +1,70 @@
 % 33 dB 绝望
 
-SIZE = 6;
-load('./dnxy.mat');
-inputName = './kitchen.ppm';
+% load('./dnxy.mat')
+inputName = './../../HEVC_like/earth.ppm';
 rgb = imread(inputName);
 yuv = jpeg_rgb2ycbcr(rgb);
 yuv = double(yuv);
-
-[yuv_cell, repeat_height, repeat_width] = img_mat2cell(yuv, SIZE);
+[yuv_cell, repeat_height, repeat_width] = img_mat2cell(yuv, 6);
 rebuild = yuv_cell;
 
+pre_ind = [1:7, 9:10, 12:25, 27:28, 30:36];
+for i = 1:numel(pre_ind)
+    [x, y] = ind2sub(6, pre_ind(i));
+
+    d1(i) = sqrt((x - 2)^2 + (y - 2)^2);
+    d2(i) = sqrt((x - 5)^2 + (y - 2)^2);
+    d3(i) = sqrt((x - 2)^2 + (y - 5)^2);
+    d4(i) = sqrt((x - 5)^2 + (y - 5)^2);
+end
+
 for i = 1:repeat_height
-    for j = i:repeat_width
+    for j = 1:repeat_width
         for k = 1:3
             mat = yuv_cell{i, j}(:, :, k);
-            p1 = mat(1, 1);
-            p2 = mat(SIZE, 1);
-            p3 = mat(1, SIZE);
-            p4 = mat(SIZE, SIZE);
+            mat_p = mat(pre_ind);
+            p1 = mat(2, 2);
+            p2 = mat(5, 2);
+            p3 = mat(2, 5);
+            p4 = mat(5, 5);
 
-            pd1x = p1 ./ d1x;
-            pd1y = p1 ./ d1y;
-            pd2x = p2 ./ d2x;
-            pd2y = p2 ./ d2y;
-            pd3x = p3 ./ d3x;
-            pd3y = p3 ./ d3y;
-            p4dx = p4 ./ d4x;
-            pd4x = p4 ./ d4x;
-            pd4y = p4 ./ d4y;
+            pd1 = p1 ./ d1;
+            pd2 = p2 ./ d2;
+            pd3 = p3 ./ d3;
+            pd4 = p4 ./ d4;
+            pd1 = pd1(:);
+            pd2 = pd2(:);
+            pd3 = pd3(:);
+            pd4 = pd4(:);
+            % pd1x = p1 ./ d1x;
+            % pd1y = p1 ./ d1y;
+            % pd2x = p2 ./ d2x;
+            % pd2y = p2 ./ d2y;
+            % pd3x = p3 ./ d3x;
+            % pd3y = p3 ./ d3y;
+            % p4dx = p4 ./ d4x;
+            % pd4x = p4 ./ d4x;
+            % pd4y = p4 ./ d4y;
+            % pd1xx = pd1x(:);
+            % pd2xx = pd2x(:);
+            % pd3xx = pd3x(:);
+            % pd4xx = pd4x(:);
+            % pd1yy = pd1y(:);
+            % pd2yy = pd2y(:);
+            % pd3yy = pd3y(:);
+            % pd4yy = pd4y(:);
 
-            pd1xx = pd1x(:);
-            pd2xx = pd2x(:);
-            pd3xx = pd3x(:);
-            pd4xx = pd4x(:);
-            pd1yy = pd1y(:);
-            pd2yy = pd2y(:);
-            pd3yy = pd3y(:);
-            pd4yy = pd4y(:);
+            X = [ones(size(pd1)) pd1 pd2 pd3 pd4];
+            b = regress(mat_p(:), X);
 
-            X = [ones(size(pd1xx)) pd1xx pd1yy pd2xx pd2yy pd3xx pd3yy pd4xx pd4yy];
-            b = regress(mat(:), X);
+            mat_fit = b(1) + b(2) * pd1 + b(3) * pd2 + b(4) * pd3 + b(5) * pd4;
 
-            mat_fit = b(1) + b(2) * pd1xx + b(3) * pd1yy + b(4) * pd2xx + b(5) * pd2yy + b(6) * pd3xx + b(7) * pd3yy + b(8) * pd4xx + b(9) * pd4yy;
-
-            rebuild{i, j}(:, :, k) = reshape(mat_fit, SIZE, SIZE);
+            rebuildmat = mat;
+            rebuildmat(pre_ind) = mat_fit;
+            rebuild{i, j}(:, :, k) = rebuildmat;
         end
     end
 end
 
-rebuild = uint8(cell2mat(rebuild));
+rebuild = jpeg_ycbcr2rgb(uint8(cell2mat(rebuild)));
 imshow(rebuild);
